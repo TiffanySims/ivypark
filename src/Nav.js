d39e555bb53ab176123ebc03b427d5038e0a9fae
Tiffany <tiffanyjewelsims@gmail.com>
2018-12-30 18:57:54 -0600

    navbar

--- src/Nav.js
+++ src/Nav.js
@@ -2,8 +2,14 @@ import React from 'react';
 import{ Link} from 'react-router-dom';
 
 const NavBar = () => {
+
+   
+
    return (
+    
+
     <div className="nav">
+    
     <Link to ="/">
     <h3 className="nav-heading">IVY PARK</h3>
     </Link>
@@ -18,6 +24,7 @@ const NavBar = () => {
         </Link>
     </ul>
     </div>
+   
    )
 }
 export default NavBar
